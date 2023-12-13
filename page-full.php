06939e996fb035967a9f3ca2a5df6a411834780a
Ray Flores <rayflores88@gmail.com>
2023-12-13 07:39:34 -0500

    remove the_content from full page template (#18)

--- page-full.php
+++ page-full.php
@@ -23,26 +23,8 @@ the_post();
 			endif;
 		endwhile;
 	endif;
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'my-theme' ) . '">',
-				'after'    => '</nav>',
-				'pagelink' => esc_html__( 'Page %', 'my-theme' ),
-			)
-		);
-		edit_post_link(
-			esc_attr__( 'Edit', 'my-theme' ),
-			'<span class="edit-link">',
-			'</span>'
-		);
 	?>
 </div><!-- /#post-<?php the_ID(); ?> -->
 <?php
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
 
 get_footer();
