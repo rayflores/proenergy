b0fa2035f29f99f1050f666e82aebfc59a524cc6
Ray Flores <rayflores88@gmail.com>
2023-12-13 00:21:39 -0500

    New Theme setup files (#16)

--- build/style.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'd5c2b94ca63553552d96');
---
+++ modules/hero/hero.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Block Name: Hero
+ * 
+ * Proenergy Hero Block Template
+ */
+?>
+<div class="hero-container">now here</div>
