97e4d8302d1ab20c25d678f992222b110feb9e2b
Ray Flores <rayflores88@gmail.com>
2023-12-13 23:19:28 -0500

    adding frontend hero template and styles

--- functions.php
+++ functions.php
@@ -565,3 +565,38 @@ function acf_json_save_point( $path ) {
     return get_stylesheet_directory() . '/acf-json';
 }
 add_filter( 'acf/settings/save_json', 'acf_json_save_point' );
+/* Hover Module Effect */
+function pe_acf_admin_head() {
+	$siteURL = get_site_url();
+	?>
+	<style type="text/css">
+	.imagePreview { position:absolute; right:100%; top:0px; z-index:999999; border:1px solid #f2f2f2; box-shadow:0px 0px 3px #b6b6b6; background-color:#fff; padding:20px;}
+	.imagePreview img { width:300px; height:auto; display:block; }
+	.acf-tooltip li:hover { background-color:#0074a9; }
+	</style>
+	<script>
+	jQuery(document).ready(function($) {
+		$('a[data-name=add-layout]').click(function(){
+			waitForEl('.acf-tooltip li', function() {
+				$('.acf-tooltip li a').hover(function(){
+					imageTP = $(this).attr('data-layout');
+					$('.acf-tooltip').append('<div class="imagePreview"><img src="<?php echo $siteURL; ?>/wp-content/themes/proenergy/modules/editor/' + imageTP + '.png"></div>');
+				}, function(){
+					$('.imagePreview').remove();
+				});
+				});
+			})
+			var waitForEl = function(selector, callback) {
+				if (jQuery(selector).length) {
+					callback();
+				} else {
+					setTimeout(function() {
+					waitForEl(selector, callback);
+				}, 100);
+			}
+		};
+	})
+	</script>
+	<?php
+	}
+	add_action('acf/input/admin_head', 'pe_acf_admin_head');
--- modules/hero/hero.php
+++ modules/hero/hero.php
@@ -4,5 +4,24 @@
  * 
  * Proenergy Hero Block Template
  */
+$hero_image = get_sub_field( 'hero_image_video' );
+$hero_headline = get_sub_field( 'hero_headline' );
 ?>
-<div class="hero-container">now here</div>
+<style>
+.hero-container {
+    max-height: 1040px;
+}
+.hero-container img.img-fluid {
+    max-height: 1040px;
+    object-fit: none;
+    object-position: 0 0; /* positioned top left of the content box */
+}    
+</style>
+<div class="hero-wrapper">
+    <div class="hero-container">
+        <?php echo wp_get_attachment_image( $hero_image['ID'], 'full', "", array( 'class' => 'img-fluid' ) ); ?>
+        <div class="hero-headline">
+            <?php echo $hero_headline; ?>
+        </div>   
+    </div>
+</div>
