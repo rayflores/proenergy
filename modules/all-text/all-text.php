5bdd1ab2988db7565f5ff41fd5dd99da124f0fb3
Ray Flores <rayflores88@gmail.com>
2024-01-02 20:21:26 -0500

    add all appropriate classes and section off modules (#26)

--- modules/all-text/all-text.php
+++ modules/all-text/all-text.php
@@ -6,65 +6,64 @@ $headline = get_sub_field('at_headline');
 $description = get_sub_field('at_description');
 $link = get_sub_field('at_link');
 ?>
-<style>
-.all-text-wrapper {
-    background-color: #ffffff;
-    color: #53565A;
-}
-.all-text-wrapper .container {
-    margin: 0;
-    padding: 4em 6em;
-}
-.all-text-container {
-    padding: 2em 0;
-}
-.all-text-headline {
-    color: #53565A;
-    font-family: 'Eurostile-Bold';
-    font-size: 2.25em;
-    letter-spacing: 0;
-    margin-bottom: 0.5em;
-}
-.all-text-description {
-    font-family: 'MyriadPro-Regular';
-    font-size: 1.125em;
-    width: 50%;
-}
-a.at-link {
-    color: #A3A8AA;
-    display: inline-block;
-    font-family: 'MyriadPro-Italic';
-    font-size: 1.125em;
-    margin-top: 30px;
-}
-a.at-link::after{
-    content: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/right-arrow.png');
-    display: inline-block;
-    width: 40px;
-    height: 0;
-    margin-left: 1em;
-}
-i.bi-arrow-right {
-    color: #A3A8AA;
-    font-size: 1.125em;
-    margin-left: 0.5em;
-}
-i.bi-arrow-right::before {
-    width: 30px;
-}
-</style>
 <section id="section_all-text">
+    <style>
+    .all-text-wrapper {
+        background-color: #ffffff;
+        color: #53565A;
+    }
+    .all-text-wrapper .container {
+        margin: 0;
+        padding: 4em 6em;
+    }
+    .all-text-container {
+        padding: 2em 0;
+    }
+    .all-text-headline {
+        color: #53565A;
+        font-family: 'Eurostile-Bold';
+        font-size: 2.25em;
+        letter-spacing: 0;
+        margin-bottom: 0.5em;
+    }
+    .all-text-description {
+        font-family: 'MyriadPro-Regular';
+        font-size: 1.125em;
+    }
+    a.at-link {
+        color: #A3A8AA;
+        display: inline-block;
+        font-family: 'MyriadPro-Italic';
+        font-size: 1.125em;
+        margin-top: 30px;
+    }
+    a.at-link::after{
+        content: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/images/right-arrow.png');
+        display: inline-block;
+        width: 40px;
+        height: 0;
+        margin-left: 1em;
+    }
+    i.bi-arrow-right {
+        color: #A3A8AA;
+        font-size: 1.125em;
+        margin-left: 0.5em;
+    }
+    i.bi-arrow-right::before {
+        width: 30px;
+    }
+    </style>
     <div class="container-fluid">
         <div class="row">
-            <div class="all-text-wrapper col-12 px-5">
-                <div class="all-text-container px-5">
+            <div class="all-text-wrapper col-12 p-5">
+                <div class="all-text-container p-5">
                     <?php
                     if ( $headline ) : ?>
                     <h2 class="all-text-headline"><?php echo $headline; ?></h2>
                     <?php endif; ?>
                     <?php
                     if ( $description ) : ?>
-                    <div class="all-text-description"><?php echo $description; ?></div>
+                    <div class="all-text-description col-6 pe-5"><?php echo $description; ?></div>
                     <?php endif; ?>
                     <?php 
                     if ( $link ) : ?>
@@ -74,4 +73,4 @@ i.bi-arrow-right::before {
             </div>
         </div>
     </div>
-</div>
+</section>
--- modules/hero/hero.php
+++ modules/hero/hero.php
@@ -7,46 +7,45 @@
 $hero_image = get_sub_field( 'hero_image_video' );
 $hero_headline = get_sub_field( 'hero_headline' );
 ?>
-<style>
-.hero-container {
-    max-height: 100%;
-    position: relative;
-}
-.hero-container img.img-fluid {
-    max-height: 880px;
-    object-fit: none;
-    object-position: 0 0; /* positioned top left of the content box */
-}
-.hero-container .hero-headline {
-    bottom: 0;
-    color: #ffffff;
-    font-family: 'Eurostile-Normal';
-    font-size: 6em;
-    letter-spacing: 15px;
-    line-height: 0.8;
-    padding: 0.5em 1em;
-    position: absolute;
-    text-transform: uppercase;
-}
-.hero-container .hero-headline::before {
-    position: absolute;
-    background: linear-gradient(to right, #003A70 192px, #862633 150px);
-    height: 20px;
-    content: '';
-    bottom: -10px;
-    right: 0;
-    left: 0;
-}
-.hero-container .hero-headline span {
-    font-family: 'Eurostile-Bold';
-}
-
-</style>
 <section id="section_hero-wrapper">
+    <style>
+    .hero-container {
+        max-height: 100%;
+        position: relative;
+    }
+    .hero-container img.img-fluid {
+        max-height: 880px;
+        object-fit: none;
+        object-position: 0 0; /* positioned top left of the content box */
+    }
+    .hero-container .hero-headline {
+        bottom: 0;
+        color: #ffffff;
+        font-family: 'Eurostile-Normal';
+        font-size: 6em;
+        letter-spacing: 15px;
+        line-height: 0.8;
+        padding: 0.5em 1em;
+        position: absolute;
+        text-transform: uppercase;
+    }
+    .hero-container .hero-headline::before {
+        position: absolute;
+        background: linear-gradient(to right, #003A70 192px, #862633 150px);
+        height: 20px;
+        content: '';
+        bottom: -10px;
+        right: 0;
+        left: 0;
+    }
+    .hero-container .hero-headline span {
+        font-family: 'Eurostile-Bold';
+    }
+    </style>
     <div class="hero-container col-lg-12">
         <?php echo wp_get_attachment_image( $hero_image['ID'], 'full', "", array( 'class' => 'img-fluid' ) ); ?>
-        <div class="hero-headline col-lg-8">
+        <div class="hero-headline col-lg-6">
             <?php echo $hero_headline; ?>
         </div>   
     </div>
-</div>
+</section>
