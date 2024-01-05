d44260686d7c1d68113ecca54902acd99be0c381
Ray Flores <rayflores88@gmail.com>
2024-01-05 10:40:21 -0500

    adding header band and tabbed modules (#29)

---
+++ modules/tabbed-bil/tabbed-bil.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Module Name: Tabbed Bulleted Image Left
+*/
+$section_bg = get_sub_field('tbil_background_grey') !== '' ? get_sub_field('tbil_background_grey') : '';
+$image_video = get_sub_field('tbil_image_video') !== '' ? get_sub_field('tbil_image_video') : '';
+$top_text = get_sub_field('tbil_top_text') !== '' ? get_sub_field('tbil_top_text') : '';
+$headline = get_sub_field('tbil_header_text') !== '' ? get_sub_field('tbil_header_text') : '';
+$paragraph = get_sub_field('tbil_paragraph') !== '' ? get_sub_field('tbil_paragraph') : '';
+$tabs = get_sub_field('tbil_tabs') !== '' ? get_sub_field('tbil_tabs') : '';
+$bullet_color = get_sub_field('tbil_bullet_color') !== '' ? get_sub_field('tbil_bullet_color') : '';
+?>
