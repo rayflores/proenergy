ccbb2e6f112d51d18a10896a9f85bd01b6f70415
Ray Flores <rayflores88@gmail.com>
2024-02-19 08:42:58 -0500

    header small image header

--- modules/about-us/about-us-header/aboutus-header-small.php
+++ modules/about-us/about-us-header/aboutus-header-small.php
@@ -6,7 +6,7 @@
 $ab_header_image_small_id = get_sub_field('ab_header_image_small');
 $ab_header_image_small_srcset = get_image_srcset($ab_header_image_small_id);
 ?>
-<section id="section_pflx-header-wrapper" class="small masthead" style="background: url('<?php echo esc_url(wp_get_attachment_url($ab_header_image_lg_id)); ?>') no-repeat center center;background-size: cover;">
+<section id="section_ab-header-wrapper" class="small masthead" style="background: url('<?php echo esc_url(wp_get_attachment_url($ab_header_image_small_id)); ?>') no-repeat center center;background-size: cover;">
 <style>
     .masthead.small {
         padding-top: 12rem;
