82d3562ae39edcb561a07d3d72b9ab54c9553ddb
Ray Flores <rayflores88@gmail.com>
2024-02-25 21:56:13 -0500

    finalizing user conf page

--- modules/about-us/about-us-reg-form/about-us-reg-form.php
+++ modules/about-us/about-us-reg-form/about-us-reg-form.php
@@ -33,15 +33,41 @@
             letter-spacing: 1px;
             color: #862633;
         }
+        .ab-reg-form-container input {
+            border-color: rgba(83, 86, 90, 0.5);
+        }
+        .ab-reg-form-container input:focus-visible {
+            outline: none;
+        }
+        .ab-reg-form-container input::placeholder {
+            color: rgba(83, 86, 90, 0.5);
+        }
         .ab-reg-form-submit-button {
             width: 100%;
             height: 50px;
             padding: 10px;
             margin: 10px 0;
             font-size: 18px;
-            border: 1px solid #53565A;
-            background-color: #53565A;
+            background-color: rgba(83, 86, 90, 0.2);
             color: #fff;
+            font-family : "MyriadPro-Italic";
+            line-height : 25px;
+            color : rgb(83, 86, 90);
+        }
+        .ab-reg-form-submit-button::after {
+            content: url('<?php echo get_stylesheet_directory_uri(); ?>/images/right-arrow.png');
+            display: inline-block;
+            width: 40px;
+            height: 0;
+            margin-left: 1em;
+        }
+        .ab-reg-form-submit-button button {
+            background-color: transparent;
+            border: none;
+            color: #53565A;
+            font-family: "MyriadPro-Italic";
+            font-size: 18px;
+            line-height: 25px;
         }
     </style>
     <div class="container-fluid p-0">
@@ -58,47 +84,47 @@
         <form method="post">
             <div class="row g-0 justify-content-end">
                 <div class="col-4"></div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-firstname-container pe-3">
-                        <input type="text" class="ab-reg-form-firstname-input border-start-0 border-top-0 border-end-0 w-100" placeholder="First Name" value="">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
+                        <input type="text" class="ab-reg-form-input border-start-0 border-top-0 border-end-0 w-100" placeholder="First Name" value="">
                     </div>
                 </div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-lastname-container pe-3">
-                        <input type="text" class="ab-reg-form-lastname-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Last Name" value="">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
+                        <input type="text" class="ab-reg-form-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Last Name" value="">
                     </div>
                 </div>
             </div>
             <div class="row g-0 justify-content-end">
                 <div class="col-4"></div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-job-title-container pe-3">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
                         <input type="text" class="ab-reg-form-job-title-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Job Title" value="">
                     </div>
                 </div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-company-container pe-3">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
                         <input type="text" class="ab-reg-form-company-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Company" value="">
                     </div>
                 </div>
             </div>
             <div class="row g-0 justify-content-end">
                 <div class="col-4"></div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-email-container pe-3">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
                         <input type="email" class="ab-reg-form-email-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Email" value="">
                     </div>
                 </div>
-                <div class="col-3 ps-2 pb-5">
-                    <div class="ab-reg-form-phone-container pe-3">
+                <div class="col-3 pb-5">
+                    <div class="ab-reg-form-container pe-3">
                         <input type="tel" class="ab-reg-form-phone-input border-start-0 border-top-0 border-end-0 w-100" placeholder="Phone" value="" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
                     </div>
                 </div>
             </div>
             <div class="row g-0">
-                <div class="col-6 pb-4 mb-5">
-                    <div class="ab-reg-form-submit-button">
-                        Submit
+                <div class="col-8 pb-4 mb-5">
+                    <div class="ab-reg-form-submit-button text-end pe-5">
+                        <button type="submit">Submit</button>
                     </div>
                 </div>
             </div>
