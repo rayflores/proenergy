ddf37db3bdb4c2d0af6448b8c4dba3530447ecf8
Ray Flores <rayflores88@gmail.com>
2024-03-10 20:50:16 -0400

    three bullets

--- modules/aero-advantage/aa-three-bullets/aa-three-bullets.php
+++ modules/aero-advantage/aa-three-bullets/aa-three-bullets.php
@@ -7,8 +7,9 @@
 <section id="section_aa-three-bullets" class="mx-5 mb-5 pb-5">
 <style>
     .aa-single-bullet-percentage {
-        font-family : "Eurostile-Bold";
+        font-family : "eurostile", sans-serif; 
         font-size : 35px;
+        font-weight : 700;
         line-height : 54.85px;
         text-transform : uppercase;
         color : #862633;
@@ -32,7 +33,7 @@
                 $aa_single_bullet_percentage = get_sub_field('aa_single_bullet_percentage') !== '' ? get_sub_field('aa_single_bullet_percentage') : '';
                 $aa_single_bullet_text = get_sub_field('aa_single_bullet_text') !== '' ? get_sub_field('aa_single_bullet_text') : '';
                 ?>
-                    <div class="aa-single-bullet col-4">
+                    <div class="aa-single-bullet col-12 col-md-4">
                         <div class="aa-single-bullet-percentage"><?php echo $aa_single_bullet_percentage; ?></div>
                         <div class="aa-single-bullet-text"><?php echo $aa_single_bullet_text; ?></div>
                     </div>
